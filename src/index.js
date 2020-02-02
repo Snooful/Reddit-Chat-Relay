feb340d4e8f2d5f36c837afb6753b4707e1d6ce1
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-02-01 23:52:26 -0500

    Add support for receiving Snoomoji

--- src/index.js
+++ src/index.js
@@ -36,6 +36,27 @@ const get = got.extend({
 	prefixUrl: config.apiHost,
 });
 
+const snoomojiPrefix = "https://www.redditstatic.com/desktop2x/img/snoomoji/";
+
+/**
+ * Gets a snoomoji URL prefix from a message.
+ * @param {SendBird.UserMessage} message The message to get the snoomoji URL prefix from.
+ * @returns {string} The snoomoji URL prefix for the message.
+ */
+function getSnoomojiURL(message) {
+	try {
+		const data = JSON.parse(message.data);
+		if (data && data.v1 && data.v1.snoomoji) {
+			const extension = data.v1.snoomoji === "partyparrot" ? "gif" : "png";
+			return snoomojiPrefix + data.v1.snoomoji + "." + extension + " ";
+		}
+
+		return "";
+	} catch (error) {
+		return "";
+	}
+}
+
 /**
  * Launch the relay.
  */
@@ -67,10 +88,12 @@ async function launch() {
 			if (channel.url !== config.channel) return;
 			if (message.sender.nickname === self.nickname) return;
 
+			const prefix = getSnoomojiURL(message);
+
 			get({
 				json: {
 					gateway: config.apiGateway,
-					text: message.message,
+					text: prefix + message.message,
 					username: message.sender.nickname,
 				},
 				method: "POST",
